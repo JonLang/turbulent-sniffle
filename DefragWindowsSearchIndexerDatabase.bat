1e37a5450772461043d1d25d679520f8b3e3ac07
JonLang <jonathan_lang@hotmail.co.uk>
2016-01-31 14:02:19 +0000

    Set variable for log path

--- DefragWindowsSearchIndexerDatabase.bat
+++ DefragWindowsSearchIndexerDatabase.bat
@@ -1,13 +1,14 @@
 @echo Off
-Echo -----------------------------START----------------------------------- >> C:\Temp\DefragWindowsSearchIndexerDatabase.log
-Echo Going to run offline Defrag of the Windows.edb (Windows Search Indexer Database) >> C:\Temp\DefragWindowsSearchIndexerDatabase.log
-FORFILES  /P %AllUsersProfile%\Microsoft\Search\Data\Applications\Windows\ /M Windows.EDB /C "cmd /c echo Pre Defrag Size @fsize KB" >> C:\Temp\DefragWindowsSearchIndexerDatabase.log
+set LogPath="C:\Temp\DefragWindowsSearchIndexerDatabase.log"
+Echo -----------------------------START----------------------------------- >> %logpath%
+Echo Going to run offline Defrag of the Windows.edb (Windows Search Indexer Database) >> %logpath%
+FORFILES  /P %AllUsersProfile%\Microsoft\Search\Data\Applications\Windows\ /M Windows.EDB /C "cmd /c echo Pre Defrag Size @fsize KB" >> %logpath%
 Sc config wsearch start=disabled
 Net stop wsearch
-echo Started defrag at %date% %time% >> C:\Temp\DefragWindowsSearchIndexerDatabase.log
-esentutl.exe /d %AllUsersProfile%\Microsoft\Search\Data\Applications\Windows\Windows.edb /t X:\tempdfrg.edb >> C:\Temp\DefragWindowsSearchIndexerDatabase.log
-echo Completed defrag at %date% %time% >> C:\Temp\DefragWindowsSearchIndexerDatabase.log
-FORFILES  /P %AllUsersProfile%\Microsoft\Search\Data\Applications\Windows\ /M Windows.EDB /C "cmd /c echo Post Defrag Size @fsize KB" >> C:\Temp\DefragWindowsSearchIndexerDatabase.log
+echo Started defrag at %date% %time% >> %logpath%
+esentutl.exe /d %AllUsersProfile%\Microsoft\Search\Data\Applications\Windows\Windows.edb /t X:\tempdfrg.edb >> %logpath%
+echo Completed defrag at %date% %time% >> %logpath%
+FORFILES  /P %AllUsersProfile%\Microsoft\Search\Data\Applications\Windows\ /M Windows.EDB /C "cmd /c echo Post Defrag Size @fsize KB" >> %logpath%
 Sc config wsearch start=delayed-auto
 Net start wsearch
-Echo -----------------------------END----------------------------------- >> C:\Temp\DefragWindowsSearchIndexerDatabase.log
+Echo -----------------------------END----------------------------------- >> %logpath%
