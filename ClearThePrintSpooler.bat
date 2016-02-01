d1e92fdab903b7ffb0d207a7ef95581484d1580e
JonLang <jonathan_lang@hotmail.co.uk>
2016-02-01 14:30:52 +0000

    Added silent delete and pause

--- ClearThePrintSpooler.bat
+++ ClearThePrintSpooler.bat
@@ -5,9 +5,9 @@ SET /P AREYOUSURE=Do you want to clear all jobs from the print sppoler (Y/[N])?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
 net stop spooler
-del %SystemRoot%\system32\spool\PRINTERS\*.*
+del %SystemRoot%\system32\spool\PRINTERS\*.* /Q
 net start spooler
-
+Pause()
 
 :END
 endlocal
